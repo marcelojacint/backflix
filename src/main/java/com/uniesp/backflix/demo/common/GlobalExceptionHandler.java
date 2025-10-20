38476b41ea223206543db96b4f2362a660cb7076
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-10-20 20:28:58 -0300

    feat: cria classe de tratamento e mensagem personalizada handler

---
+++ src/main/java/com/uniesp/backflix/demo/common/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.uniesp.backflix.demo.common;
+
+import com.uniesp.backflix.demo.exception.EntidadeNaoEncontradaException;
+import com.uniesp.backflix.demo.service.dtos.erro.ErroCampo;
+import com.uniesp.backflix.demo.service.dtos.erro.ErroResposta;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        List<FieldError> fieldErrors = e.getFieldErrors();
+
+        List<ErroCampo> listaErros = fieldErrors
+                .stream()
+                .map(fie -> new ErroCampo(fie.getField(), fie.getDefaultMessage())).toList();
+
+        return new ErroResposta(HttpStatus.UNPROCESSABLE_ENTITY.value(), " de validaçãerroo", listaErros);
+
+    }
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErroResposta handlerRegistroDuplicadoException(EntidadeNaoEncontradaException e) {
+        return ErroResposta.conflito(e.getMessage());
+    }
+}
